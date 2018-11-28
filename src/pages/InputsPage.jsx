4747da880f5419dbd7165f8654c85b5b20e86dfc
Phil Hagger <philip.hagger@gmail.com>
2018-11-28 11:37:32 +0000

    - added padding to the inputs page

--- src/pages/InputsPage.jsx
+++ src/pages/InputsPage.jsx
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Input, TextArea } from '../components/Input/Input';
 
 const InputsPage = () => (
-  <Fragment>
+  <div style={{ padding: '10px' }}>
     <h3>Inputs</h3>
     <h5>Input</h5>
     <Input />
     <h5>TextArea</h5>
     <TextArea defaultValue="This is a text area" />
-  </Fragment>
+  </div>
 );
 
 export default InputsPage;
